88eb532dc1fc0df75da9921c37672aa70c300f11
jiyi <jiyi_lari@yahoo.com>
2017-09-25 22:42:35 +0900

    automatically get network service name, not assuming wi-fi anymore

--- run_me.sh
+++ run_me.sh
@@ -4,12 +4,40 @@ function print_log()
 	echo $1 >> helper.log
 }
 
+function get_network_service_name()
+{
+	services=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
+
+	while read line; do
+	    sname=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $2}')
+	    sdev=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $4}')
+	    #echo "Current service: $sname, $sdev, $currentservice"
+	    if [ -n "$sdev" ]; then
+	        ifout="$(ifconfig $sdev 2>/dev/null)"
+	        echo "$ifout" | grep 'status: active' > /dev/null 2>&1
+	        rc="$?"
+	        if [ "$rc" -eq 0 ]; then
+	            currentservice="$sname"
+	        fi
+	    fi
+	done <<< "$(echo "$services")"
+
+	if [ -n "$currentservice" ]; then
+	    echo $currentservice
+	else
+	    print_log "No network service, quitting"
+	    exit 1
+	fi
+}
+
+network_service_name=`get_network_service_name`
+
 print_log "The change been made can be recovered at: System Preference -> Network -> Advanced -> Proxies -> Automatic Proxy Configuration."
 
-if [ ! `networksetup -getautoproxyurl wi-fi | grep NeteaseMusic | wc -l | xargs` = "1" ] || [ ! `networksetup -getautoproxyurl wi-fi | grep Yes | wc -l | xargs` = "1" ] 
+if [ ! `networksetup -getautoproxyurl $network_service_name | grep NeteaseMusic | wc -l | xargs` = "1" ] || [ ! `networksetup -getautoproxyurl $network_service_name | grep Yes | wc -l | xargs` = "1" ] 
 then
 	print_log "Password is needed only for the first time."
-	networksetup -setautoproxyurl wi-fi file://`pwd`/NeteaseMusic.pac
+	networksetup -setautoproxyurl $network_service_name file://`pwd`/NeteaseMusic.pac
 fi
 
 nohup python NeteaseMusicProxy.py > /dev/null &
